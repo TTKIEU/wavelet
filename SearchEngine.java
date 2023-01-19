22f8e9a524657a4160ac071a28ceefd11d7044ee
TTKIEU <taylorrr787909@gmail.com>
2023-01-19 12:30:33 -0800

    a

--- SearchEngine.java
+++ SearchEngine.java
@@ -7,22 +7,25 @@ import java.net.URI;
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    StringBuilder i;
     String test= "test";
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Taylin's string: %d",test);
+            //return String.format("Taylin's string: %d",test);
+            String newTest= String.format("taylin's string: %8s",test);
+            return newTest;
         } else if (url.getPath().equals("/addA")) {
-            i.append("A");
-            return String.format("Letter A added!");
+            test=test+"A";
+            return String.format("Letter A added! %8s",test);
         } else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("string")) {
-                    i.append(parameters);
-                    return String.format("Your new String list is: ", i);
+                    for (int i=0; i<parameters.length; i++){
+                    test=test+parameters[i];
+                    }
+                    return String.format("Your new String list is: %8s", test);
                 }
             }
             return "404 Not Found!";
